eb6330c449ca1fe6e61d854b406a47efe2e2f439
LambertLan <landingyu@163.com>
2018-04-17 00:20:48 +0800

    record各个模块主要页面完成

--- data_system/urls.py
+++ data_system/urls.py
@@ -6,6 +6,11 @@ from data_system import views
 urlpatterns = [
     path('', views.Index.as_view()),
     path('index.html', views.Index.as_view()),
-    path('public_data.html', views.PublicData.as_view()),
+    path('public_data/', views.PublicData.as_view()),
+    path('debt_search/', views.DebtSearch.as_view()),
+    path('customize_report/', views.CustomizeReport.as_view()),
+    path('account_information/', views.AccountInformation.as_view()),
+    path('financial_information/', views.FinancialInformation.as_view()),
+    path('select_records/', views.SelectRecords.as_view()),
 
 ]
--- data_system/views.py
+++ data_system/views.py
@@ -3,6 +3,7 @@ from django import views
 
 
 # Create your views here.
+# 账户首页
 class Index(views.View):
     def get(self, request):
         return render(request, "record/index.html", {"active": "index"})
@@ -11,6 +12,37 @@ class Index(views.View):
         return render(request, "record/index.html", {"active": "index"})
 
 
+# 公众数据获取
 class PublicData(views.View):
     def get(self, request):
         return render(request, "record/public_data.html", {"active": "public_data"})
+
+
+# 负债搜索报告
+class DebtSearch(views.View):
+    def get(self, request):
+        return render(request, "record/debt_search.html", {"active": "debt_search"})
+
+
+# 定制版报告
+class CustomizeReport(views.View):
+    def get(self, request):
+        return render(request, "record/customize_report.html", {"active": "customize_report"})
+
+
+# 账户信息
+class AccountInformation(views.View):
+    def get(self, request):
+        return render(request, "record/account_information.html", {"active": "account_information"})
+
+
+# 财务信息
+class FinancialInformation(views.View):
+    def get(self, request):
+        return render(request, "record/financial_information.html", {"active": "financial_information"})
+
+
+# 查询记录
+class SelectRecords(views.View):
+    def get(self, request):
+        return render(request, "record/select_records.html", {"active": "select_records"})
